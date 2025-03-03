a1280b5e78dbaae3c6b0459a66c0ab3286b692f3
khorrlybobo <koladeemmanuela32@gmail.com>
2025-03-04 00:32:13 +0100

    removed unnessary spaces

--- src/run_diner.sh
+++ src/run_diner.sh
@@ -11,7 +11,7 @@ weight_decay=0.01
 learning_rate=5e-5
 max_len_s=120
 max_len_a=13
-model_name=Afro_xlmr 
+model_name=Afro_xlmr
 
 if [ $Counterfactual = 1 ];then
     if [ $ARTS = 1 ];then
@@ -42,4 +42,4 @@ python3 main_cfabsa.py \
     --max_len_s $max_len_s \
     --max_len_a $max_len_a \
     --save_dir $save_dir 2>&1 | tee  ${save_dir}/run.log \
-    --model_name $model_name 
+    --model_name $model_name
